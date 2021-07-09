5f336e30ec496b4aa3e8fc701b3d369ae896ece8
JaanbaazAkhtar <jaanbaazakhtar786@gmail.com>
2021-07-10 00:49:46 +0530

    Tesla Clone completed

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -1,19 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-const HomeSection = () => {
+const HomeSection = ({title, description,  leftBtnText, rightBtnText, backgroundImg}) => {
     return(
         <>
-            <Wrap>
+            <Wrap bgImage={backgroundImg}>
                 <ItemText>
-                    <h1>Model S</h1>
-                    <p>Order Online for tochless delivery</p>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
                 </ItemText>
 
                 <Buttons>
                     <ButtonGroup>
-                        <LeftButton>Custom Order</LeftButton>
-                        <RightButton>Existing Inventory</RightButton>
+                        <LeftButton>{leftBtnText}</LeftButton>
+                        { rightBtnText &&
+                            <RightButton>{rightBtnText} </RightButton>
+                        }
+                        
                     </ButtonGroup>
 
                     <DownArrow src="/images/down-arrow.svg"/>
@@ -32,11 +35,11 @@ const Wrap = styled.div`
     background-size:cover;
     background-position: center;
     background-repeat: no-repeat;
-    background-image: url('/images/model-s.jpg');
     display: flex;
     flex-direction: column;
     justify-content:space-between;
     align-items:center;
+    background-image: ${props =>`url("/images/${props.bgImage}")`}
 `
 
 const ItemText = styled.div`
@@ -47,6 +50,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
     display:flex;
     margin-bottom: 30px;
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 
 const LeftButton = styled.div`
@@ -65,12 +71,15 @@ const LeftButton = styled.div`
     margin: 8px;
 `
 const RightButton = styled(LeftButton)`
-
+    background:white;
+    opacity: 0.65;
+    color:black;
 `
 
 const DownArrow = styled.img`
-    margin-top: 20px;
     height: 40px;
+    overflow-x: hidden;
+    animation: animateDown infinite 1.5s;
 `
 
 const Buttons = styled.div`
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,7 +6,61 @@ const Home = () => {
     return(
         <>
             <Container>
-                <HomeSection />
+                <HomeSection 
+                    title="Model S"
+                    description="Order Online for Touchless Delivery"
+                    backgroundImg="model-s.jpg"
+                    leftBtnText="Custom order"
+                    rightBtnText="Existing inventory" 
+                />
+
+                <HomeSection 
+                    title="Model Y"
+                    description="Order Online for Touchless Delivery"
+                    backgroundImg="model-y.jpg"
+                    leftBtnText="Custom order"
+                    rightBtnText="Existing inventory" 
+                />
+
+                <HomeSection 
+                    title="Model 3"
+                    description="Order Online for Touchless Delivery"
+                    backgroundImg="model-3.jpg"
+                    leftBtnText="Custom order"
+                    rightBtnText="Existing inventory" 
+                />
+
+                <HomeSection 
+                    title="Model X"
+                    description="Order Online for Touchless Delivery"
+                    backgroundImg="model-x.jpg"
+                    leftBtnText="Custom order"
+                    rightBtnText="Existing inventory" 
+                />
+
+                <HomeSection 
+                    title="Lowest Cost Solar Panel in India"
+                    description="Money-back guarantee"
+                    backgroundImg="solar-panel.jpg"
+                    leftBtnText="Order order"
+                    rightBtnText="Learn More" 
+                />
+
+                <HomeSection 
+                    title="Solar for New Roofs"
+                    description="Solar Roofs cost less than New Roofs"
+                    backgroundImg="solar-roof.jpg"
+                    leftBtnText="Order order"
+                    rightBtnText="Learn More" 
+                />
+
+                <HomeSection 
+                    title="Accessories"
+                    description=""
+                    backgroundImg="accessories.jpg"
+                    leftBtnText="Shop Now" 
+                />
+
             </Container>
         </>
     )
